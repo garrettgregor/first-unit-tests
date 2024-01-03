09151ff4265b2ff675915fe7af336f9ee700f21c
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-03 15:27:49 -0700

    feat: sayHello

--- functions.js
+++ functions.js
@@ -3,6 +3,14 @@ function addTwoNumbers(num1, num2) {
     return num1 + num2
 };
 
+function sayHello(name) {
+    if (name === "Will") {
+        return `No more testing ${name}!`
+    } else {
+        return `Hi there ${name}!`
+    }
+}
+
 module.exports = {
     addTwoNumbers,
     sayHello,
